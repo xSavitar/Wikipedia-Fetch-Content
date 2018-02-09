dd88d765f2200ee70226a8d573350d7d06a00a8e
African Hope <samuelguebo@gmail.com>
2018-02-09 13:54:09 +0000

    autocomplete backbone

--- script.js
+++ script.js
@@ -10,43 +10,61 @@ $(document).ready(function () {
 	$('#fetch').click(function(e){
 		e.preventDefault();
 		var url = wikipedia_fetch_url();
-        $.ajax( {
-            type: "GET",
-            url: url,
-            dataType: 'jsonp',
-            success: function(data) {
-            	console.log(url);
-            	if(data.hasOwnProperty('parse')){
-	            	//Hide default title placeholder and show title after search
-	                $('#title').hide();
-	                document.getElementById('title').innerHTML = data.parse.title;
-	                $('#title').show();
+		console.log(url);
+		$.ajax( {
+			type: "GET",
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				console.log(url);
+				if(data.hasOwnProperty('parse')){
+					//Hide default title placeholder and show title after search
+					$('#title').hide();
+					document.getElementById('title').innerHTML = data.parse.title;
+					$('#title').show();
 
-	                //Show fetched content after the search
-	                $('#content').hide();
-	                document.getElementById('content').innerHTML = data.parse.text['*'];
-	                $('#content').show();
-	            } else {
-	            	$('#title').hide();
-	                document.getElementById('title').innerHTML = "Error Occured :(";
-	                $('#title').show();
+					//Show fetched content after the search
+					$('#content').hide();
+					document.getElementById('content').innerHTML = data.parse.text['*'];
+					$('#content').show();
+				} else {
+					$('#title').hide();
+					document.getElementById('title').innerHTML = "Error Occured :(";
+					$('#title').show();
 
-	                $('#content').hide();
-	                document.getElementById('content').innerHTML = "Article doesn't exist in selected language or error occured!";
-	                $('#content').show();
-	            }
-          	}
-        });
+					$('#content').hide();
+					document.getElementById('content').innerHTML = "Article doesn't exist in selected language or error occured!";
+					$('#content').show();
+				}
+			}
+		});
 	});
+
+	/* Fire autocomplete feature on keyup */
+	$('#article_name').keyup(function(e)){
+		
+	}
 });
 
 /* JS function to build the Fetch URL */
 function wikipedia_fetch_url() {
 	var article_name = $('#article_name').val();
 	var language = $('#language').val();
-    var base_url = "https://" + language + ".wikipedia.org/w/api.php";
-    var data_format = "&format=json";
-    var request_url = "?action=parse&prop=text&page=" + article_name;
-    var url = base_url + request_url + data_format;
-    return url;
+	var base_url = "https://" + language + ".wikipedia.org/w/api.php";
+	var data_format = "&format=json";
+	var request_url = "?action=parse&prop=text&page=" + article_name;
+	var url = base_url + request_url + data_format;
+	return url;
 }
+
+/* Autocomplete
+function wikipedia_autocomplte_title() {
+	var article_name = $('#article_name').val();
+	var language = $('#language').val();
+	var base_url = "https://" + language + ".wikipedia.org/w/api.php";
+	var data_format = "&format=json";
+	var request_url = "?action=parse&prop=text&page=" + article_name;
+	var url = base_url + request_url + data_format;
+	return url;
+}
+*/
